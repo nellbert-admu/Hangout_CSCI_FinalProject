021448b6adae846790b6865f90bb5885a00136fa
nellbert-admu <167055842+nellbert-admu@users.noreply.github.com>
2024-07-17 21:23:19 +0800

    refactored place names

--- app/src/main/java/com/example/hangout_csci_finalproject/Place.java
+++ app/src/main/java/com/example/hangout_csci_finalproject/Place.java
@@ -7,12 +7,12 @@ public class Place extends RealmObject {
     @PrimaryKey
     private String id;
     private String name;
-    private boolean hasDining;
-    private boolean hasOutlet;
-    private boolean hasAircon;
-    private boolean hasQuiet;
-    private boolean hasRestroom;
-    private boolean hasWifi;
+    private boolean dining;
+    private boolean outlet;
+    private boolean aircon;
+    private boolean quiet;
+    private boolean restroom;
+    private boolean wifi;
 
     // Getters and setters
     public String getId() { return id; }
@@ -21,21 +21,51 @@ public class Place extends RealmObject {
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
 
-    public boolean isHasDining() { return hasDining; }
-    public void setHasDining(boolean hasDining) { this.hasDining = hasDining; }
+    public boolean isDining() {
+        return dining;
+    }
 
-    public boolean isHasOutlet() { return hasOutlet; }
-    public void setHasOutlet(boolean hasOutlet) { this.hasOutlet = hasOutlet; }
+    public void setDining(boolean dining) {
+        this.dining = dining;
+    }
 
-    public boolean isHasAircon() { return hasAircon; }
-    public void setHasAircon(boolean hasAircon) { this.hasAircon = hasAircon; }
+    public boolean isOutlet() {
+        return outlet;
+    }
 
-    public boolean isHasQuiet() { return hasQuiet; }
-    public void setHasQuiet(boolean hasQuiet) { this.hasQuiet = hasQuiet; }
+    public void setOutlet(boolean outlet) {
+        this.outlet = outlet;
+    }
 
-    public boolean isHasRestroom() { return hasRestroom; }
-    public void setHasRestroom(boolean hasRestroom) { this.hasRestroom = hasRestroom; }
+    public boolean isAircon() {
+        return aircon;
+    }
 
-    public boolean isHasWifi() { return hasWifi; }
-    public void setHasWifi(boolean hasWifi) { this.hasWifi = hasWifi; }
+    public void setAircon(boolean aircon) {
+        this.aircon = aircon;
+    }
+
+    public boolean isQuiet() {
+        return quiet;
+    }
+
+    public void setQuiet(boolean quiet) {
+        this.quiet = quiet;
+    }
+
+    public boolean isRestroom() {
+        return restroom;
+    }
+
+    public void setRestroom(boolean restroom) {
+        this.restroom = restroom;
+    }
+
+    public boolean isWifi() {
+        return wifi;
+    }
+
+    public void setWifi(boolean wifi) {
+        this.wifi = wifi;
+    }
 }
