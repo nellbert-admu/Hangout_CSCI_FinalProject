c8774bb49f2f90bd2c8cfb3c534991e7bfc0eb2f
mristl <lyutzm@gmail.com>
2024-07-19 16:22:47 +0800

    placedetail help

--- app/src/main/java/com/example/hangout_csci_finalproject/PlaceDetail.java
+++ app/src/main/java/com/example/hangout_csci_finalproject/PlaceDetail.java
@@ -1,34 +1,49 @@
 package com.example.hangout_csci_finalproject;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import io.realm.Realm;
 
 public class PlaceDetail extends AppCompatActivity {
-
+    private TextView placeNameView, locationView, descriptionView;
+    private ToggleButton toggleDining, toggleOutlets, toggleAircon, toggleQuiet, toggleRestrooms, toggleWifi;
+    private RatingBar ratingBar;
+    private String placeId;
     private Realm realm;
-    private TextView placeNameView;
-    private TextView locationView;
-    private TextView descriptionView;
-    private ToggleButton toggleDining;
-    private ToggleButton toggleOutlets;
-    private ToggleButton toggleAircon;
-    private ToggleButton toggleQuiet;
-    private ToggleButton toggleRestrooms;
-    private ToggleButton toggleWifi;
-    private Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_placedetail);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        realm = Realm.getDefaultInstance();
+        initViews();
+        placeId = getIntent().getStringExtra("placeId");
+        if (placeId != null) {
+            loadPlaceData(placeId);
+        }
 
+        findViewById(R.id.backButton).setOnClickListener(v -> finish());
+    }
+
+    private void initViews() {
         placeNameView = findViewById(R.id.placeName);
         locationView = findViewById(R.id.placeLocation);
         descriptionView = findViewById(R.id.placeDescription);
@@ -38,40 +53,29 @@ public class PlaceDetail extends AppCompatActivity {
         toggleQuiet = findViewById(R.id.toggleQuiet);
         toggleRestrooms = findViewById(R.id.toggleRestrooms);
         toggleWifi = findViewById(R.id.togglePlaceWifi);
-        backButton = findViewById(R.id.backButton);
-
-        realm = Realm.getDefaultInstance();
-
-        String placeId = getIntent().getStringExtra("placeId");
-        if (placeId != null) {
-            Place place = realm.where(Place.class).equalTo("id", placeId).findFirst();
-            if (place != null) {
-                placeNameView.setText(place.getName());
-                toggleDining.setChecked(place.isDining());
-                toggleOutlets.setChecked(place.isOutlet());
-                toggleAircon.setChecked(place.isAircon());
-                toggleQuiet.setChecked(place.isQuiet());
-                toggleRestrooms.setChecked(place.isRestroom());
-                toggleWifi.setChecked(place.isWifi());
-            }
-        }
-
-        backButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                onBackButtonClicked(view);
-            }
-        });
+        ratingBar = findViewById(R.id.ratingBar);
     }
 
-    public void onBackButtonClicked(View view) {
-        finish();
+    private void loadPlaceData(String placeId) {
+        Place place = realm.where(Place.class).equalTo("id", placeId).findFirst();
+        if (place != null) {
+            placeNameView.setText(place.getName());
+            locationView.setText(place.getLocation());
+            descriptionView.setText(place.getDescription());
+            toggleDining.setChecked(place.isDining());
+            toggleOutlets.setChecked(place.isOutlet());
+            toggleAircon.setChecked(place.isAircon());
+            toggleQuiet.setChecked(place.isQuiet());
+            toggleRestrooms.setChecked(place.isRestroom());
+            toggleWifi.setChecked(place.isWifi());
+            ratingBar.setRating(place.getRating());
+        }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if (realm != null) {
+        if (!realm.isClosed()) {
             realm.close();
         }
     }
