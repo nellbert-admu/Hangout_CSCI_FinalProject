40a44dd04a4dcf50fbc7b14f122736de4b80ae06
nellbert-admu <167055842+nellbert-admu@users.noreply.github.com>
2024-07-19 11:49:55 +0800

    fixed back button

--- app/src/main/java/com/example/hangout_csci_finalproject/PlaceDetail.java
+++ app/src/main/java/com/example/hangout_csci_finalproject/PlaceDetail.java
@@ -1,12 +1,13 @@
 package com.example.hangout_csci_finalproject;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.ToggleButton;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import io.realm.Realm;
 
 public class PlaceDetail extends AppCompatActivity {
@@ -64,8 +65,6 @@ public class PlaceDetail extends AppCompatActivity {
     }
 
     public void onBackButtonClicked(View view) {
-        Intent intent = new Intent(this, Home.class);
-        startActivity(intent);
         finish();
     }
 
